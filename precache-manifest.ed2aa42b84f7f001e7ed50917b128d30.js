b1d979e5c54694649f38cd6283ff7dcb54941add
HBKarim <find_yourlove2@yahoo.com>
2019-01-08 07:48:50 +0200

    Updates

---
+++ precache-manifest.ed2aa42b84f7f001e7ed50917b128d30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/page1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9285ce60faf2bd1837e8",
+    "url": "/page1/static/js/runtime~main.9285ce60.js"
+  },
+  {
+    "revision": "c48a65d0396492ee7583",
+    "url": "/page1/static/js/main.c48a65d0.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/page1/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "c48a65d0396492ee7583",
+    "url": "/page1/static/css/main.68b33f3a.chunk.css"
+  },
+  {
+    "revision": "be26caf3293937290d10a65c703b3bad",
+    "url": "/page1/index.html"
+  }
+];
